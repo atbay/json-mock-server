aa7a8c58aaca4612697d67eaeeda78fc8e927ef5
atbay418\atbay <atbay418@gmail.com>
2018-09-14 22:43:17 +0900

    test用API追加

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
+import { Request, Response } from 'express';
 
-import { Telemetory } from '../model/telemetory';
+import { Telemetory } from '../controller/telemetory';
 
 const ver1: string = 'v1';
 
@@ -10,12 +11,18 @@ export class Router {
 
     public roters(app: express.Application) :void {
 
+        // test api
+        app.route(`/`)
+        .get(( req: Request, res: Response) => {
+            res.json({'message': 'It Works!!'});
+        });
+
         // telmetory api
         app.route(`${ver1}/telemetory`)
         .get(this.telemetory.get)
         .post(this.telemetory.post)
         .delete(this.telemetory.delete);
-
+        
         app.route(`${ver1}/telemetory:id`)
         .get(this.telemetory.getById)
         .put(this.telemetory.put)
