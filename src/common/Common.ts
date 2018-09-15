af4076c4874b203da849e9197f29974073f593a4
atbay418\atbay <atbay418@gmail.com>
2018-09-16 00:12:44 +0900

    post、delete追加

--- src/common/Common.ts
+++ src/common/Common.ts
@@ -2,21 +2,26 @@ import * as fs from 'fs';
 
 export class Common {
 
-    public static readJson(filepath: string): Promise<any> {
-        return new Promise((resolve) => {
+    public static async readJson(filepath: string): Promise<any> {
+        return new Promise((resolve, reject) => {
             fs.readFile(filepath, 'utf8', (err: NodeJS.ErrnoException, data: string) => {
-                resolve(JSON.parse(data));
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(JSON.parse(data));
+                }
             });
         });
     }
 
     public static async writeJson(filepath: string, data: string): Promise<{}> {
-
-        const json: any = await Common.readJson(filepath);
-        console.log(json);
-        return new Promise((resolve) => {
-            fs.writeFile(filepath, 'utf8', (err: NodeJS.ErrnoException) => {
-                resolve();
+        return new Promise((resolve, reject) => {
+            fs.writeFile(filepath, data, (err: NodeJS.ErrnoException) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
             });
         });
     }
--- src/controller/telemetory.ts
+++ src/controller/telemetory.ts
@@ -6,29 +6,70 @@ const dbpath: string = 'db/data.json';
 
 export class Telemetory {
 
+    /**
+     * リソース取得(all)
+     * @param request request
+     * @param response response
+     * @param next next
+     */
     public async get(request: Request, response: Response, next: NextFunction) {
         const json: any = await Common.readJson(dbpath);
-        console.log(json[0].id);
         response.status(200).send(json);
     }
 
+    /**
+     * リソース取得(ID)
+     * @param request request
+     * @param response response
+     * @param next next
+     */
     public async getById(request: Request, response: Response, next: NextFunction) {
-        const json: any = await Common.readJson(dbpath);
-        if (json) {
-            const id = request.param('id');
-            for (const index in json) {
-                console.log(json[index]);
-                if (json[index].id === id) {
-                    response.status(200).send(json[index]);
+        try {
+            const json: any = await Common.readJson(dbpath);
+            if (json) {
+                const id = request.param('id');
+                for (const index in json) {
+                    if (json[index].UtmId === id) {
+                        const resJson = json[index];
+                        resJson.Result = 'Success';
+                        response.status(200).send(resJson);
+                        break;
+                    }
                 }
+                return;
             }
+            response.status(404).send({ message: 'Not Found.' });
+        } catch (error) {
+            console.log(error);
+            response.status(500).send({ message: 'Internal Server Error.' });
         }
 
-        response.status(404).send({ message: 'not found' });
     }
 
+    /**
+     * リソース追加
+     * @param request request
+     * @param response response
+     * @param next next
+     */
     public async post(request: Request, response: Response, next: NextFunction) {
-        response.send('OK!');
+        if (request.headers['content-type'] !== 'application/json') {
+            response.status(400).send({
+                message: `Bad Content-type [${request.headers['content-type'] }]`,
+            });
+            return;
+        }
+
+        try {
+            const json: any = await Common.readJson(dbpath);
+            if (json) {
+                json.push(request.body);
+            }
+            Common.writeJson(dbpath, JSON.stringify(json, undefined, 1)); // 整形した形で出力
+            response.status(201).send('OK!');
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     public async put(request: Request, response: Response, next: NextFunction) {
@@ -36,6 +77,24 @@ export class Telemetory {
     }
 
     public async delete(request: Request, response: Response, next: NextFunction) {
-        response.send('called delete method!');
+        try {
+            const json: any = await Common.readJson(dbpath);
+            if (json) {
+                const id = request.param('id');
+                for (const index in json) {
+                    if (json[index].UtmId === id) {
+                        json.splice(index, 1);
+                        await Common.writeJson(dbpath, JSON.stringify(json));
+                        response.status(200).send();
+                        break;
+                    }
+                }
+                return;
+            }
+            response.status(200).send();
+        } catch (error) {
+            console.log(error);
+            response.status(500).send({ message: 'Internal Server Error.' });
+        }
     }
 }
